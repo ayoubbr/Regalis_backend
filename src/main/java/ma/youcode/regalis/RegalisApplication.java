52023a1496757fe85384313a7298d3904eb20efb
unknown <ayoub1benmansour@gmail.com>
2026-01-18 15:38:20 +0100

    first commit, added docker compose with mysql and phpmyadmin, added spring data, spring web, lombok and mysql dependencies, added readme file

---
+++ src/main/java/ma/youcode/regalis/RegalisApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.regalis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegalisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegalisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/youcode/regalis/RegalisApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.regalis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegalisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
